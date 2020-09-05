2cece23b194f5848b4808bf838a445e4b2d596c3
Borcze Angelov <borcze.angelov@yahoo.com>
2020-09-05 15:49:06 +0200

    Add project files.

---
+++ Dispatcher_Demo01/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace Dispatcher_Demo01
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1Seconds_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button3Seconds_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button5Seconds_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
