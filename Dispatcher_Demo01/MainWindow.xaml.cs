55546ca04d0364e603d0e8702bdecb3aee22be52
Borcze Angelov <borcze.angelov@yahoo.com>
2020-09-05 16:01:19 +0200

    Add simple event logging

--- Dispatcher_Demo01/MainWindow.xaml.cs
+++ Dispatcher_Demo01/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace Dispatcher_Demo01
 {
@@ -11,17 +12,29 @@ namespace Dispatcher_Demo01
 
         private void Button1Seconds_Click(object sender, RoutedEventArgs e)
         {
-
+            LogBegin(nameof(Button1Seconds));
         }
 
         private void Button3Seconds_Click(object sender, RoutedEventArgs e)
         {
-
+            LogBegin(nameof(Button3Seconds));
         }
 
         private void Button5Seconds_Click(object sender, RoutedEventArgs e)
         {
+            LogBegin(nameof(Button5Seconds));
+        }
 
+        private void LogBegin(string buttonName)
+        {
+            var timeStamp = GetTimeStamp();
+            var eventName = $"\"{buttonName}\" is clicked";
+            LogList.Items.Add(timeStamp + " " + eventName);
+        }
+
+        private static string GetTimeStamp()
+        {
+            return DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff");
         }
     }
 }
