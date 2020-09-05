bfd3ee80b23dd6ad946a1805d3e5d216eee9247e
Borcze Angelov <borcze.angelov@yahoo.com>
2020-09-05 16:04:14 +0200

    Add ThreadSleep for each button click event

--- Dispatcher_Demo01/MainWindow.xaml.cs
+++ Dispatcher_Demo01/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Windows;
 
 namespace Dispatcher_Demo01
@@ -13,16 +14,19 @@ namespace Dispatcher_Demo01
         private void Button1Seconds_Click(object sender, RoutedEventArgs e)
         {
             LogBegin(nameof(Button1Seconds));
+            Thread.Sleep(1000);
         }
 
         private void Button3Seconds_Click(object sender, RoutedEventArgs e)
         {
             LogBegin(nameof(Button3Seconds));
+            Thread.Sleep(3000);
         }
 
         private void Button5Seconds_Click(object sender, RoutedEventArgs e)
         {
             LogBegin(nameof(Button5Seconds));
+            Thread.Sleep(5000);
         }
 
         private void LogBegin(string buttonName)
