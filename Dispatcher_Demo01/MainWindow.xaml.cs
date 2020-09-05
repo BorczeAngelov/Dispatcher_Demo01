4325203f17ac49560201eabfac1f5c31d432762d
Borcze Angelov <borcze.angelov@yahoo.com>
2020-09-05 16:06:50 +0200

    Log when button operation is completed

--- Dispatcher_Demo01/MainWindow.xaml.cs
+++ Dispatcher_Demo01/MainWindow.xaml.cs
@@ -15,24 +15,34 @@ namespace Dispatcher_Demo01
         {
             LogBegin(nameof(Button1Seconds));
             Thread.Sleep(1000);
+            LogEnd(nameof(Button1Seconds));
         }
 
         private void Button3Seconds_Click(object sender, RoutedEventArgs e)
         {
             LogBegin(nameof(Button3Seconds));
             Thread.Sleep(3000);
+            LogEnd(nameof(Button3Seconds));
         }
 
         private void Button5Seconds_Click(object sender, RoutedEventArgs e)
         {
             LogBegin(nameof(Button5Seconds));
             Thread.Sleep(5000);
+            LogEnd(nameof(Button5Seconds));
         }
 
         private void LogBegin(string buttonName)
         {
             var timeStamp = GetTimeStamp();
             var eventName = $"\"{buttonName}\" is clicked";
+            LogList.Items.Add(timeStamp + " " + eventName);
+        }
+
+        private void LogEnd(string buttonName)
+        {
+            var timeStamp = GetTimeStamp();
+            var eventName = $"\"{buttonName}\" completed its operation";
             LogList.Items.Add(timeStamp + " " + eventName);
         }
 
